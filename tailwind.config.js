8ef2f8256ee8f9f743ce4e12097615732ed3608b
Ashwin Chadaga <ashwinchadaga@gmail.com>
2026-02-05 15:23:42 -0500

    Add typography plugin configuration to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.slate.300'),
+            h1: { color: theme('colors.slate.100') },
+            h2: { color: theme('colors.slate.100') },
+            h3: { color: theme('colors.slate.200') },
+            strong: { color: theme('colors.emerald.400') },
+            a: { color: theme('colors.blue.400') },
+            blockquote: { 
+              borderLeftColor: theme('colors.red.900'),
+              color: theme('colors.slate.400')
+            },
+          },
+        },
+      }),
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
